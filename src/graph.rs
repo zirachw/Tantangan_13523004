ccb58ca6cc0f3bff7d7adbb597dcfdd7f4e61362
Razi <zirachwidh@gmail.com>
2025-06-02 15:47:10 +0700

    feat: Graph data structure

---
+++ src/graph.rs
@@ -0,0 +1,64 @@
+pub struct Graph 
+{
+    n: usize,
+    #[allow(dead_code)]
+    city_names: Vec<String>,
+    adj_matrix: Vec<Vec<i32>>,
+}
+
+impl Graph 
+{
+    pub fn new(n: usize, city_names: Vec<String>, adj_matrix: Vec<Vec<i32>>) -> Self 
+    {
+        Graph 
+        {
+            n,
+            city_names,
+            adj_matrix,
+        }
+    }
+    
+    pub fn get_graph_info(&self) -> (usize, usize) 
+    {
+        let num_roads = self.count_roads();
+        let starting_node = 0;
+        (num_roads, starting_node)
+    }
+    
+    fn count_roads(&self) -> usize 
+    {
+        let mut count = 0;
+        for i in 0..self.n 
+        {
+            for j in 0..self.n 
+            {
+                if i != j && self.adj_matrix[i][j] > 0 
+                {
+                    count += 1;
+                }
+            }
+        }
+        count
+    }
+    
+    pub fn get_n(&self) -> usize 
+    {
+        self.n
+    }
+    
+    pub fn get_distance(&self, i: usize, j: usize) -> i32 
+    {
+        self.adj_matrix[i][j]
+    }
+    
+    #[allow(dead_code)]
+    pub fn get_city_name(&self, index: usize) -> &str 
+    {
+        &self.city_names[index]
+    }
+    
+    pub fn has_edge(&self, i: usize, j: usize) -> bool 
+    {
+        self.adj_matrix[i][j] > 0
+    }
+}
